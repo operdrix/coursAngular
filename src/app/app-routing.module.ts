2e706aa896b33e65b66a96622c433598368e017a
Olivier PERDRIX <olivierperdrix@live.fr>
2023-08-28 15:16:14 +0200

    Mise en pratique - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainTaskComponent } from './main-task/main-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tasks', component: MainTaskComponent },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
